59af77ca7fde670c43b3aaa7d7bcc39a7d7c666f
Ozan Öztuna <ozan.oztuna@adayazilim.com>
2024-04-27 04:29:35 +0300

    Deneme commiti

--- components/Contacts/contacts.jsx
+++ components/Contacts/contacts.jsx
@@ -7,7 +7,7 @@ function Contacts() {
     <section className='w-full flex flex-col items-center gap-0'>
     <SectionHeading>Contacts</SectionHeading>
     <div className='flex items-center justify-center gap-4 my-8'>
-    <p><a href="tel:+905074213927">+90 111 111 11 11</a></p>
+    <p><a href="tel:+905074213927">+90 222 222 22 22</a></p>
     <div className='flex items-center justify center bg-gray-800 h-10 w-[1px] hidden sm:block'></div>
     <address>500 Terry Francine Street<br></br>San Francisco, CA94158</address>
     </div>
